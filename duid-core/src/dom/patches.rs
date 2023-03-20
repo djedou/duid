61c6ef9c993530eadebfd9099229d3b988f284d1
djedou <djedouarnaud.aman@gmail.com>
2023-03-20 15:35:33 +0100

    patches

--- duid-core/src/dom/patches.rs
+++ duid-core/src/dom/patches.rs
@@ -104,32 +104,32 @@ where
                         patch_node(&old_level.1, &new_level.1, old_arena, new_arena);
                     },
                     Ordering::Less => {
-                        /*let mut new_level_vec = new_level.1.clone();
+                        let mut new_level_vec = new_level.1.clone();
                         let new_level_vec_remains = new_level_vec.split_off(old_level.1.len());
                         patch_node(&old_level.1, &new_level_vec, old_arena, new_arena);
                         new_level_vec_remains.iter().for_each(|id| {
                             mark_inserted_state(id.clone(), old_arena, &new_arena);
                             mark_children_added_state::<MSG>(&[id.clone()], old_arena, new_arena);
-                        });*/
+                        });
                     },
                     Ordering::Greater => {
-                        /*let mut old_level_vec = old_level.1.clone();
+                        let mut old_level_vec = old_level.1.clone();
                         let old_level_vec_remains = old_level_vec.split_off(new_level.1.len());
                         patch_node(&old_level_vec, &new_level.1, old_arena, new_arena);
                         old_level_vec_remains.iter().for_each(|id| {
                             mark_parent_removed_state(id.clone(), old_arena);
                             mark_children_removed_state::<MSG>(&[id.clone()], old_arena);
-                        });*/
+                        });
                     }
                 }
             });
-/*
+
             old_levels_clone_vec.iter().for_each(|(level, old_level_ids)| {
                 old_level_ids.iter().for_each(|id| {
                     mark_parent_removed_state(id.clone(), old_arena);
                     mark_children_removed_state::<MSG>(&[id.clone()], old_arena);
                 });
-            });*/
+            });
         }
     }
 }
